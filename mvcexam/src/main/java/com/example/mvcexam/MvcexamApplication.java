4e6353820478022ee17d15de3b698e22feccbc3d
ssoyeong <jsy171771@gmail.com>
2023-06-26 13:15:34 +0900

    Feat: MVC 실습 Initial Commit

---
+++ mvcexam/src/main/java/com/example/mvcexam/MvcexamApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvcexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcexamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcexamApplication.class, args);
+	}
+
+}
---
+++ mvcexam/src/test/java/com/example/mvcexam/MvcexamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvcexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcexamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
